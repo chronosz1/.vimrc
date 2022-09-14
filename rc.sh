d5ba2eab2691a33408710392d31601437187f501
chronosz1 <63775894+chronosz1@users.noreply.github.com>
2022-09-14 22:19:47 +0530

    Update rc.sh

--- rc.sh
+++ rc.sh
@@ -2,5 +2,6 @@ sudo apt install git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source ~/.vim/buldle/
 vim
-:PlugInstall
+echo 'open vim and type the below command in vim normal mode-'
+echo ':PlugInstall'
  
